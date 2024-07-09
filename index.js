39d06ecea6abba43a4b924d39fb9d3a61bb8fe42
rb-isiaq <isiaqridwanbukola1999@gmail.com>
2024-07-09 11:34:45 +0100

    added /api to the suth route

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(cors());
 
 app.use("/", indexRoutes);
 app.use("/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/organisations", organisationRoutes);
 
